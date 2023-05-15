1cf90ba9c78ba604e77ef4968db713a9df25c581
Raymond Glass <raymond.allen.glass@gmail.com>
2023-05-15 06:55:11 -0500

    hello

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,12 +3,12 @@ public class HelloWorld {
         System.out.println("Hello, World!");
 
 //  Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
-        int MyFavoriteNumber = 51;
-        System.out.println(MyFavoriteNumber);
+//        int MyFavoriteNumber = 51;
+//        System.out.println(MyFavoriteNumber);
 
 //  Create a String variable named myString and assign a string value to it, then print the variable out to the console.
-        String myString = "3.14159";
-        System.out.println(myString);
+//        String myString = "3.14159";
+//        System.out.println(myString);
 //      String myString = "Test" --> worked logged test
 
 //  Change your code to assign a character value to myString. What do you notice?
@@ -18,8 +18,8 @@ public class HelloWorld {
     //  String myString = "3.14159" -- worked inside of string "" only
 
 //  Declare a long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
-        float myNumber = 3.14f;
-        System.out.println(myNumber);
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
     //  Does not recognize the variable.
 
 //  Change your code to assign the value 3.14 to myNumber. What do you notice?
@@ -37,14 +37,47 @@ public class HelloWorld {
 //      System.out.println(x);
         // Resulted in 5 and 6 respectively
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 //  Resulted in 6 and 6 respectively - on the second one the add was performed before the input was accepted
 
 //  Try to create a variable named class. What happens?
+//        String class = "test";
+        // results in error - not a statement
+
+// Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        //cannot be cast to class java.lang.Integer
+
+// Copy and paste the code above and then run it. Does the result match with your expectation?
+//How is the above example different from the code block below?
+//        int three = (int) "three";
+
+        //incompatible types: java.lang.String cannot be converted to int
+//What are the two different types of errors we are observing?
+
+// Rewrite the following expressions using the relevant shorthand assignment operators:
+        int x = 4;
+        x = x + 5;
 
 
+        int x = 3;
+        int y = 4;
+        y = y * x;
+
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+
+// What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+//
+//Hint: Integer.MAX_VALUE is a class .,lconstant (we'll learn more about these later) that holds the maximum value for the int type.
 
     }
 }
