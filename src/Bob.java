f269c34c4be20b42fbb4926d3aca2f8b9db31912
Raymond Glass <raymond.allen.glass@gmail.com>
2023-05-17 15:56:44 -0500

    test

--- src/Bob.java
+++ src/Bob.java
@@ -3,14 +3,17 @@ public class Bob {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Hi, I am Bob...");
-        String answer;
+
+        boolean confirm;
 
         do {
+
             System.out.println("Interact with me!");
+
             String userInput = sc.nextLine();
 
             if(userInput.trim().endsWith("?")){
-                    System.out.println("Sure");
+                System.out.println("Sure");
             } else if (userInput.trim().endsWith("!")){
                 System.out.println("Whoa, chill out!");
             } else if(userInput.length() == 0){
@@ -19,29 +22,28 @@ public class Bob {
                 System.out.println("Whatever.");
             }
 
-            System.out.println("Would you like to continue [y/n]");
-            answer = sc.next();
+            System.out.println("Would you like to continue? [y/n]");
+            confirm = sc.nextLine().equalsIgnoreCase("y");
 
-            sc.nextLine();
 
-        } while(answer.equalsIgnoreCase("y"));
-        System.out.println("Thank You");
+        } while(confirm);
+        System.out.println("Thank You!");
     }
 }
+
+
+
 //    public static void main(String[] args){
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Hi, I am Bob...");
-//
-//        boolean confirm = true;
+//        String answer;
 //
 //        do {
-//
 //            System.out.println("Interact with me!");
-//
 //            String userInput = sc.nextLine();
 //
 //            if(userInput.trim().endsWith("?")){
-//                System.out.println("Sure");
+//                    System.out.println("Sure");
 //            } else if (userInput.trim().endsWith("!")){
 //                System.out.println("Whoa, chill out!");
 //            } else if(userInput.length() == 0){
@@ -50,11 +52,12 @@ public class Bob {
 //                System.out.println("Whatever.");
 //            }
 //
-//            System.out.println("Would you like to continue? [y/n]");
-//            confirm = sc.next().equalsIgnoreCase("y");
+//            System.out.println("Would you like to continue [y/n]");
+//            answer = sc.next();
 //
+//            sc.nextLine();
 //
-//        } while(confirm);
-//        System.out.println("Thank You!");
+//        } while(answer.equalsIgnoreCase("y"));
+//        System.out.println("Thank You");
 //    }
 //}
