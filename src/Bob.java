76338492af593f87498200874324896c11d71045
Raymond Glass <raymond.allen.glass@gmail.com>
2023-05-17 15:50:56 -0500

    working

--- src/Bob.java
+++ src/Bob.java
@@ -3,13 +3,10 @@ public class Bob {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Hi, I am Bob...");
-
-        boolean confirm = true;
+        String answer;
 
         do {
-
             System.out.println("Interact with me!");
-
             String userInput = sc.nextLine();
 
             if(userInput.trim().endsWith("?")){
@@ -22,11 +19,42 @@ public class Bob {
                 System.out.println("Whatever.");
             }
 
-            System.out.println("Would you like to continue? [y/n]");
-            confirm = sc.next().equalsIgnoreCase("y");
+            System.out.println("Would you like to continue [y/n]");
+            answer = sc.next();
 
+            sc.nextLine();
 
-        } while(confirm);
-        System.out.println("Thank You!");
+        } while(answer.equalsIgnoreCase("y"));
+        System.out.println("Thank You");
     }
 }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Hi, I am Bob...");
+//
+//        boolean confirm = true;
+//
+//        do {
+//
+//            System.out.println("Interact with me!");
+//
+//            String userInput = sc.nextLine();
+//
+//            if(userInput.trim().endsWith("?")){
+//                System.out.println("Sure");
+//            } else if (userInput.trim().endsWith("!")){
+//                System.out.println("Whoa, chill out!");
+//            } else if(userInput.length() == 0){
+//                System.out.println("Fine. Be that way!");
+//            } else {
+//                System.out.println("Whatever.");
+//            }
+//
+//            System.out.println("Would you like to continue? [y/n]");
+//            confirm = sc.next().equalsIgnoreCase("y");
+//
+//
+//        } while(confirm);
+//        System.out.println("Thank You!");
+//    }
+//}
