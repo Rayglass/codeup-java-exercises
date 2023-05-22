bc4c0cb50b21ca1045b838d30ffeab4b34d06b38
Raymond Glass <raymond.allen.glass@gmail.com>
2023-05-22 10:49:52 -0500

    exercise cleanup

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        addition(4, 3);
+//        addition(4, 3);
 //        subtraction(9, 2);
 //        multiplication(3, 3);
 //        division(12, 5);
@@ -11,8 +11,6 @@ public class MethodsExercises {
 //        getInteger(1, 3);
 //        factorial();
 //        diceRoll();
-//        sayHi(3);
-
     }
 
     // 1. Basic Arithmetic
@@ -56,83 +54,82 @@ public class MethodsExercises {
             System.out.println("Your number is invalid!");
             return getInteger(min, max);
         }
-        System.out.printf("your number is %s", input);
-            return input;
+        System.out.printf("your number is %s ", input);
+        return input;
     }
 
     // 3. Factorial of a Number
-
-    public static void factorial() {
-        Scanner Scanner = new Scanner(System.in);
-        System.out.println("Pick a number between 1 and 10:");
-        int input = Scanner.nextInt();
-        System.out.println("you picked number " + input);
-        System.out.println("Do you want to continue [y/n]");
-        String answer = Scanner.next();
-
-        if (answer.equals("y")) {
-
-            int sum = 1;
-            for (int i = 1; i <= input; i++) {
-                sum *= i;
-                System.out.println(sum);
-            }
-        }
-    }
-
-//        public static void factorial(){
-//            Scanner Scanner = new Scanner(System.in);
-//            System.out.println("Please enter a number between 1 and 10: ");
-//            String userIntStr = Scanner.nextLine();
-//            int userInt = Integer.parseInt(userIntStr);
-//            if (!(userInt >= 1 && userInt <= 10)){
-//                factorial();
-//            }
-//
-//            String choice;
-//            do {
-//                for (long i = 1; i <= userInt; i++) {
-//                    long factorial = i;
-//                    System.out.printf("%s! = 1 ", factorial);
-//                    for (long l = 1; l <= i; l++) {
-//                        factorial *= l;
-//                        if (l != 1) {
-//                            System.out.printf("x %s ", l);
-//                        }
-//                    }
-//                    System.out.printf("  = %s%n", factorial);
-//                }
-//                System.out.println("Would you like to continue? [y/N]");
-//                choice = Scanner.nextLine();
-//
-//            } while (choice.equalsIgnoreCase("y"));
+//    public static void factorial() {
+//        Scanner Scanner = new Scanner(System.in);
+//        System.out.println("Please enter a number between 1 and 10: ");
+//        String userIntStr = Scanner.nextLine();
+//        int userInt = Integer.parseInt(userIntStr);
+//        if (!(userInt >= 1 && userInt <= 10)) {
+//            factorial();
 //        }
+//
+//        String choice;
+//        do {
+//            for (long i = 1; i <= userInt; i++) {
+//                long factorial = i;
+//                System.out.printf("%s! = 1 ", factorial);
+//                for (long l = 1; l <= i; l++) {
+//                    factorial *= l;
+//                    if (l != 1) {
+//                        System.out.printf("x %s ", l);
+//                    }
+//                }
+//                System.out.printf("  = %s%n", factorial);
+//            }
+//            System.out.println("Would you like to continue? [y/N]");
+//            choice = Scanner.nextLine();
+//
+//        } while (choice.equalsIgnoreCase("y"));
+//    }
 
+        public static void factorial() {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Please enter a number between 1 and 10: ");
+
+        int number = Scanner.nextInt();
+            long factorial = calculateFactorial(number);
+
+                System.out.println("Factorial of " +number + " is: " + factorial);
+        }
+
+            private static long calculateFactorial(int number) {
+                if (number < 0) {
+                    throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+                }
+
+                if (number == 0 || number == 1) {
+                    return 1;
+                }
+
+                long factorial = 1;
+                for (int i = 2; i <= number; i++) {
+                    factorial *= i;
+                }
+
+                return factorial;
+            }
+
+
+
+
+//
 
     // 4. DICE ROLL
-    public static void diceRoll (){
+    public static void diceRoll() {
         Scanner Scanner = new Scanner(System.in);
         System.out.println("Give me a number of sides for a pair of dice: ");
         String diceSidesStr = Scanner.nextLine();
         int diceSides = Integer.parseInt(diceSidesStr);
         System.out.printf("Your first di rolled a %s.%nYour second di rolled a %s", randomNum(diceSides), randomNum(diceSides));
     }
-    public static int randomNum (int num){
-        return (int)((Math.random() * (num)) + 1);
+
+    public static int randomNum(int num) {
+        return (int) ((Math.random() * (num)) + 1);
     }
-
-
-// Recursive Practice
-
-    public static void sayHi(int count) {
-        System.out.println("Hi!");
-
-        if (count <= 1) {
-            return;
-        }
-        sayHi(count -1);
-    }
-
-
 
 }
