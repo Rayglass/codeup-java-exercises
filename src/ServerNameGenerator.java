036151c7b4a09da3c60f13bdffc26fcb31c07055
Raymond Glass <raymond.allen.glass@gmail.com>
2023-05-23 13:48:49 -0500

    serverName

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,23 @@
 public class ServerNameGenerator {
+
+    private static final String[] adjectives = {"automated", "twisted", "reliable", "effective", "powerful", "revolutionary", "mind-boggling", "decentralized", "dynamic", "hybrid"};
+    private static final String[] nouns = {"dodge", "chevy", "ford", "honda", "toyota", "gmc", "bmw", "mercedes", "lincoln", "cadillac"};
+
+    private static String getRandomElement(String[] array) {
+        int index = (int) (Math.random() * array.length);
+        return array[index];
+    }
+
+    public static void main(String[] args) {
+        String adjective = getRandomElement(adjectives);
+        String noun = getRandomElement(nouns);
+        String serverName = adjective + "-" + noun;
+        System.out.println("Your server name is: " + serverName);
+    }
 }
+
+
+
+
+
+
